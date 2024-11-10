a23a91814e9ffc7c858784c7a4302114b9963929
Aslihan Aria Celik <aslihancelik27@gmail.com>
2024-11-09 22:22:24 -0500

    done

--- Ch5_Exercises_StudentLineUp.cpp
+++ Ch5_Exercises_StudentLineUp.cpp
@@ -1,45 +1,55 @@
 // Ch5_Exercises_StudentLineUp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // Chapter 5 Exercise 14
-// This program prompts the user to enter the number of students in the class, then loops to read that many names.
+// This program reads the student names from the file LineUp.txt., then outputs the first and last in line.
+//
+//Programmer: Aslihan Aria Celik
 
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-	int numberOfStudents;
 	string firstInLine;
 	string currentName;
 	string lastInLine;
 
-	//User prompted to enter the number of students in the class
-	//Input Validation: Do not accept a number less than 1 or greater than 25 for the number of students.
-	do {
-		cout << "Please enter the number of the students in the class." << endl;
-		cin >> numberOfStudents;
+	ifstream infile("LineUp.txt");  // Open the file for reading
 
-		if (numberOfStudents < 1 || numberOfStudents>25) {
-			cout << "The number of students can not be less than 1 and can not be greater than 25." << endl;
+	// check if the file opened successfully to avoid runtime errors.
+	if (!infile) {
+		cerr << "Error opening file" << endl;
+		return 1; // Exit if the file cannot be opened
+	}
+	bool firstEntry = true;
+
+	//as it can read
+	while (infile >> currentName) {
+		//check for the first entry
+		if (firstEntry) { 
+			firstInLine = currentName; 
+			lastInLine = currentName; 
+			firstEntry = false; }
+		//if not first entry
+		else { 
+			//compare and update first in line
+			if (currentName < firstInLine) { 
+				firstInLine = currentName; 
+			}
+			//compare and update last in line
+			if (currentName > lastInLine) { 
+				lastInLine = currentName; 
+			}
 		}
-	} while (numberOfStudents < 1 || numberOfStudents>25);
-
-	cout << "Please enter the names of the students with a space in between the names , in example: Sally Leo Theodore" << endl;
-
-	// Initialize firstInLine and lastInLine with the first student's name 
-	cin >> firstInLine;
-	lastInLine = firstInLine;
-
-	for (int i = 1; i < numberOfStudents; i++) {
-		cin >> currentName;
-		if (currentName < firstInLine) {
-			firstInLine = currentName;
-		}
-		if (currentName > lastInLine) {
-			lastInLine = currentName;
-		}
+		// Ignore any extra newline or spaces after the name 
+		infile.ignore(); 
 	}
 
+	// Close the file after reading
+	infile.close();
+
+	//Output the result
 	cout << "The student at the front of the line is: " << firstInLine << endl;
 	cout << "The student at the end of the line is: " << lastInLine << endl;
 
